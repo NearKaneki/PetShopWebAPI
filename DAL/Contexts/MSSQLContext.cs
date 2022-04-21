81e4e3a634c009a88a9584f32d3f4abe5ee284bf
NearKaneki <near55@yandex.ru>
2022-04-22 01:49:04 +0400

    change connectionString

--- DAL/Contexts/MSSQLContext.cs
+++ DAL/Contexts/MSSQLContext.cs
@@ -10,15 +10,12 @@ namespace DAL
 {
     public  class MSSQLContext: DbContext
     {
-        public MSSQLContext(DbContextOptions<MSSQLContext> options) : base(options) 
-        {
-            
-        }
+        public MSSQLContext(DbContextOptions<MSSQLContext> options) : base(options) { }
         public DbSet<Booking> Booking { get; set; }
-        public DbSet<Cathegory> Cathegorie { get; set; }
+        public DbSet<Cathegory> Cathegory { get; set; }
         public DbSet<Client> Client { get; set; }
         public DbSet<Item> Item { get; set; }
         public DbSet<Parameter> Parameter { get; set; }
-        public DbSet<SubCathegory> SubCathegorie { get; set; }
+        public DbSet<SubCathegory> SubCathegory { get; set; }
     }
 }
--- PetShopWebAPI/Program.cs
+++ PetShopWebAPI/Program.cs
@@ -1,14 +1,17 @@
 using DAL;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddScoped<IRepo, MSSQLRepo>();
+
 builder.Services.AddDbContext<MSSQLContext>(o =>
-    o.UseSqlServer("Server=NARUT0\\MSSQLSERVER01; Database=PetShopDB; Trusted_Connection=True; MultipleActiveResultSets=true;"));
+    o.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL")));
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
